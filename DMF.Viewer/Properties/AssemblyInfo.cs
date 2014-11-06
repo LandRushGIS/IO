d2b7024f026d6c913d16ee660d48d2549fb23e7e
Nipheris <nipheris@live.ru>
2014-11-06 03:52:34 +0300

    Update assembly information to feed NuGet with proper package description and authors

--- DMF.Viewer/Properties/AssemblyInfo.cs
+++ DMF.Viewer/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LandRush.IO.DMF.Viewer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("LandRush GIS IO - sample DMF viewer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SouthTech")]
 [assembly: AssemblyProduct("LandRush")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
